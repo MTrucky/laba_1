4edf4577a4043d0f799c23f5ba2ee870376db48a
MTrucky <146337304+MTrucky@users.noreply.github.com>
2024-02-19 20:27:48 +0500

    Update 00.py

--- 00.py
+++ 00.py
@@ -1,22 +1,21 @@
 import math
-
+from math import sqrt
 sites = {
     'Moscow': (550,  370),
     'London': (510,  510),
     'Paris': (480,  480),
 }
 
-def calculate_distance(coord1, coord2):
-    x1, y1 = coord1
-    x2, y2 = coord2
-    return math.sqrt((x1 - x2) **  2 + (y1 - y2) **  2)
-
 distances = {}
-for city1, coords1 in sites.items():
-    distances[city1] = {}
-    for city2, coords2 in sites.items():
-        if city1 != city2:
-            distance = calculate_distance(coords1, coords2)
-            distances[city1][city2] = distance
 
-print(distances)
+for city1 in sites:
+    for city2 in sites:
+        if city1 != city2:
+            x1, y1 = sites[city1]
+            x2, y2 = sites[city2]
+            distance = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+            distances[f'{city1}-{city2}'] = distance
+
+# заполнение словаря distances
+for key, value in distances.items():
+    print(key, value)
